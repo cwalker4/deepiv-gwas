500b7476c2556047f227aeba880a5d7d590a77f9
billyf <billyf@stanford.edu>
2018-02-25 21:53:21 -0800

    add regularizes

--- milestone/one_stage_nn.py
+++ milestone/one_stage_nn.py
@@ -31,11 +31,17 @@ def OneStageModel(input_shape):
 
 	# Define 3 hidden layers with nodes 128, 64, 32 respectively 
 	X = Dense(128, activation='relu', name='fc1')(X)
+	Dropout(dropout_rate)
 	X = Dense(64, activation='relu', name='fc2')(X)
+	Dropout(dropout_rate)
 	X = Dense(32, activation='relu', name='fc3')(X)
+	Dropout(dropout_rate)
 
 	# Define output layer
 	X = Dense(1, activation = 'relu', name = 'output')(X)
+
+	# Define Regularization technique on activity
+	activity_regularizer = keras.regularizers.l2(l2_reg)
 
 	# Create Model
 	model = Model(inputs = X_input, outputs = X, name='OneStageModel')
