a1cc57590ab1abb50d114583c947543bcce16f23
billyf <billyf@stanford.edu>
2018-02-28 14:17:01 -0800

    as a function so that it can be called in other docs

--- scripts/clean_id_crosswalk.R
+++ scripts/clean_id_crosswalk.R
@@ -1,14 +1,17 @@
 library(here)
 library(dplyr)
 library(tidyr)
-file <- "crosswalk.txt"
 
-crosswalk <- read.csv(here::here("raw_data", file))
+clean_id_crosswalk <- function() {
 
-crosswalk %>%
-  separate(paste(colnames(crosswalk)), into = c("id", "filename", "md5", "size", "state"), 
-           extra = 'drop', sep = "\t", remove = TRUE) -> crosswalk
+  file <- "crosswalk.txt"
 
-length(unique(crosswalk$id))
-length(unique(crosswalk$filename))
-length(unique(crosswalk$md5))
+  crosswalk <- read.csv(here::here("raw_data", file))
+
+  crosswalk %>%
+    separate(paste(colnames(crosswalk)), into = c("id", "filename", "md5", "size", "state"), 
+             extra = 'drop', sep = "\t", remove = TRUE) -> crosswalk
+  
+  return(crosswalk)
+}
+
