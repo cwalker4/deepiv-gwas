e0f60c2921979bf1bf7c182e8e7ef37d0244aa6e
Charlie Walker <cwalker4@stanford.edu>
2018-03-04 18:02:53 -0800

    tidying, documenting

--- scripts/clean_TCGA.R
+++ scripts/clean_TCGA.R
@@ -14,7 +14,6 @@ crosswalk <- read_delim(here::here("raw_data/gdc_manifest.txt"), delim = '\t')
 no_ids <- crosswalk[6085:6258,]
 crosswalk <- crosswalk[1:6084,]
 
-# Figure out what's going on relating all these file and folder names
 # Each row in the Manifest.txt has a folder and then a filename. That filename is broken up into some strange 
 # 'identifier' and then a suffix broken apart by three periods
 
@@ -36,7 +35,7 @@ mRNA_data_sep %>%
   mutate(filename = paste(identifier,a, b, c, sep = ".")) %>%
   select(folder = id, filename) -> mRNA_data
 
-# unpacking and joining TCGA data
+# unpacking and joining TCGA data (takes a little while)
 get_expression <- function(x) {
   folder <- as.character(x[1])
   filename <- as.character(x[2])
@@ -47,18 +46,8 @@ get_expression <- function(x) {
 }
 
 full_data <- do.call(cbind, apply(mRNA_data, MARGIN = 1, get_expression))
-full_data <- full_data[, !duplicated(colnames(full_data))]
+full_data <- full_data[, !duplicated(colnames(full_data))] # dropping duplicate "Gene" columns
 write_csv(full_data, here::here("derived_data", "cleaned_TCGA_mRNA.csv"))
-
-# #THIS IS TOO SLOW, SOMEHOW NEED TO VECTORIZE (LAPPLY)
-# full_data <- data.frame('Gene' = 'filler')
-# for(i in 1:nrow(mRNA_data)) {
-#   obv <- read_delim(here::here("raw_data", "TCGA", as.character(mRNA_data[i,"folder"]), as.character(mRNA_data[i,"filename"])), 
-#                     delim = '\t', 
-#                     col_names = c("Gene", as.character(mRNA_data[i,"folder"])))
-#   full_data <- full_join(full_data, obv, 'Gene')
-#   print(i)
-# }
 
 #=================
 # Cleaning the sequencing data
@@ -71,14 +60,10 @@ mut_data_sep %>%
   mutate(filename = paste(identifier,a, b, c, sep = ".")) %>%
   select(folder, filename) -> mut_data
 
-print(mut_data_sep$b)
-
 gene_varscan <- read.maf(here::here("raw_data", "TCGA", as.character(mut_data[1,1]), as.character(mut_data[1,2])))
 gene_somaticsniper <- read.maf(here::here("raw_data", "TCGA", as.character(mut_data[2,1]), as.character(mut_data[2,2])))
 gene_mutect <- read.maf(here::here("raw_data", "TCGA", as.character(mut_data[3,1]), as.character(mut_data[3,2])))
 gene_muse <- read.maf(here::here("raw_data", "TCGA", as.character(mut_data[4,1]), as.character(mut_data[4,2])))
-
-as.character(mut_data[i,1])
 
 
 #=================
