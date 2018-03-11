731b0a94ada84f4cf37f34bb618d606482366c86
billyf <billyf@stanford.edu>
2018-03-11 16:44:03 -0700

    clean gtex mrna

--- scripts/clean_gtex_mrna.R
+++ scripts/clean_gtex_mrna.R
@@ -1,6 +1,11 @@
 library(here)
 library(CePa)
 library(tidyverse)
-filepath <- here::here("GTEx", "All_Tissue_Site_Details.combined.reads.gct")
+filepath <- here::here("raw_data", "GTEx", "All_Tissue_Site_Details.combined.reads.gct")
 X <- read.gct(filepath)
 gtex <- as.data.frame(X)
+
+gtex %>%
+  mutate(Gene = rownames(gtex)) -> gtex
+
+write_csv(gtex, here::here("derived_data", "cleaned_gtex_mrna.csv"))
