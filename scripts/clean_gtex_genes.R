b527052b62a57e8c56ad11242bd077dcbd61bd46
Charlie Walker <cwalker4@stanford.edu>
2018-03-11 17:29:12 -0700

    fixing directory path

--- scripts/clean_gtex_genes.R
+++ scripts/clean_gtex_genes.R
@@ -3,13 +3,13 @@ library(biomaRt)
 library(tidyverse)
 library(dplyr)
 
-data <- read_csv(here::here("raw_data", "GTEx", "raw_gtex_genes.csv"))
+data <- read_csv(here::here("raw_data/gtex/raw_gtex_genes.csv"))
 
-rs_hugo_cross <- read_csv(here::here("crosswalks", "rsids_to_hugo.csv"))
+rs_hugo_cross <- read_csv(here::here("crosswalks/rsids_to_hugo.csv"))
 
 data %>%
   merge(rs_hugo_cross, by = 'X1') %>%
-  select(-rs_id_dbSNP147_GRCh37p13.x, -rs_id_dbSNP147_GRCh37p13.y, -gene_id) %>%
+  dplyr::select(-rs_id_dbSNP147_GRCh37p13.x, -rs_id_dbSNP147_GRCh37p13.y, -gene_id) %>%
   rename(hugo = gene_name) -> data_hugo
 
 write_csv(data_hugo, here::here("derived_data", "cleaned_gtex_genes.csv"))
