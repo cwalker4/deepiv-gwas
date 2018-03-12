77082ae5d06e3882718fd8509496c98281bc4cf5
billyf <billyf@stanford.edu>
2018-03-11 20:12:20 -0700

    finish up data merging

--- scripts/combine_data.R
+++ scripts/combine_data.R
@@ -7,16 +7,6 @@ gtex_genes <- read_csv(here::here("derived_data/cleaned_gtex_genes.csv"))
 tcga_mrna <- read_csv(here::here("derived_data/cleaned_tcga_mrna.csv"))
 gtex_mrna <- read_csv(here::here("derived_data/cleaned_gtex_mrna.csv"))
 
-
-
-### Join on hugo to create our Z matrix
-gtex_genes %>%
-  inner_join(tcga_genes, by = 'hugo') %>%
-  group_by(hugo) %>%
-  summarize_all(sum) -> genes
-
-### Join on mrna (Ensemble ids) to create our P matrix
-
 # don't need info after the period
 delete_extra_info <- function(data) {
   data %>%
@@ -29,7 +19,20 @@ delete_extra_info <- function(data) {
 gtex_mrna <- delete_extra_info(gtex_mrna)
 tcga_mrna <- delete_extra_info(tcga_mrna)
 
+tcga_mrna %>%
+  select(names(tcga_genes[,2:279]),  mrna) -> tcga_mrna
+
+### Join on hugo to create our Z matrix
+gtex_genes %>%
+  inner_join(tcga_genes, by = 'hugo') %>%
+  group_by(hugo) %>%
+  summarize_all(sum) -> genes
+
+### Join on mrna (Ensemble ids) to create our P matrix
+
 gtex_mrna %>%
   inner_join(tcga_mrna, by = 'mrna') -> mrna
 
+write_csv(genes, here::here("derived_data", "gene_variants.csv"))
+write_csv(mrna, here::here("derived_data", "expression_levels.csv"))
 
