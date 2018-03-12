bee23e14da5e94f702453af1ed1131b56e1249b2
billyf <billyf@stanford.edu>
2018-03-11 18:01:35 -0700

    comments

--- scripts/combine_data.R
+++ scripts/combine_data.R
@@ -1,17 +1,21 @@
 library(here)
 library(tidyverse)
 
+
 tcga_genes <- read_csv(here::here("derived_data/cleaned_tcga_genes.csv"))
 gtex_genes <- read_csv(here::here("derived_data/cleaned_gtex_genes.csv"))
 tcga_mrna <- read_csv(here::here("derived_data/cleaned_tcga_mrna.csv"))
 gtex_mrna <- read_csv(here::here("derived_data/cleaned_gtex_mrna.csv"))
 
 
+
+### Join on hugo to create our Z matrix
 gtex_genes %>%
   inner_join(tcga_genes, by = 'hugo') %>%
   group_by(hugo) %>%
   summarize_all(sum) -> genes
 
+### Join on mrna (Ensemble ids) to create our P matrix
 
 
 
