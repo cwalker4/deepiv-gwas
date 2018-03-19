ee66db1a0f7438e71b512bc5d3f18efb6dff4cdb
Charlie Walker <cwalker4@stanford.edu>
2018-03-18 19:04:36 -0700

    print statements through data_loader

--- analysis/model/data_loader.py
+++ analysis/model/data_loader.py
@@ -26,16 +26,22 @@ def load_data(self, types, stage, data_dir):
             if stage == 'treatment':
                 expression_path = os.path.join(data_dir, stage, split, "expression.csv")
                 variants_path = os.path.join(data_dir, stage, split, "variants.csv")
+                print("Loading expression...")
                 expression = np.loadtxt(expression_path, delimiter=',')
+                print("- done.\nLoading variants...")
                 variants = np.loadtxt(variants_path, delimiter=',')
+                print("- done")
                 data[split]['data'] = expression
                 data[split]['labels'] = variants
                 data[split]['size'] = variants.shape[0]
             elif stage == 'response':
                 expression_path = os.path.join(data_dir, stage, "expression.csv")
                 outcomes_path = os.path.join(data_dir, stage, split, "outcomes.csv")
+                print("Loading expression...")
                 expression = np.loadtxt(expression_path, delimiter=',')
+                print("- done.\nLoading outcomes...")
                 outcomes = np.loadtxt(loutcomes_path, delimiter=',')
+                print("- done.")
                 data[split]['data'] = expression
                 data[split]['labels'] = outcomes
                 data[split]['size'] = expression.shape[0]
