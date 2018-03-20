5068d8205fc95fc39d6312fa34bb64d23e011b81
billyf <billyf@stanford.edu>
2018-03-20 07:34:37 -0700

    Output decision tree in pdf

--- analysis/decision_tree.py
+++ analysis/decision_tree.py
@@ -11,17 +11,25 @@ import pydotplus
 
 print("Loading expression...")
 expression_levels = pd.read_csv('data/simulate/expression.csv')
+#expression_levels = pd.read_csv('data/treatment/test/expression.csv')
 print("- done.\nLoading outcomes...")
 outcomes = pd.read_csv('data/simulate/outcomes.csv')
+#outcomes = pd.read_csv('data/response/test/outcomes.csv')
 print("- done.")
 
-decision_tree = tree.DecisionTreeClassifier(max_depth = 5, min_samples_leaf = 10)
+decision_tree = tree.DecisionTreeClassifier(max_depth = 10)
 print("Fitting tree...")
 decision_tree = decision_tree.fit(expression_levels, outcomes)
 print("- done.")
 
-dot_data = tree.export_graphviz(decision_tree, out_file='data/simulate/tree_raw.pdf') 
-graph = pydotplus.graph_from_dot_data(dot_data) 
-graph.write_pdf("data/simulate/tree_processed.pdf")
-#graph = graphviz.Source(dot_data) 
-#graph.render('data/simulate/tree.gv', view = True) 
+print(">>>>> Trained fruit_classifier <<<<<")
+print(decision_tree)
+
+
+with open("figures/decision_tree.dot", "w") as f:
+    f = tree.export_graphviz(decision_tree, out_file=f)
+
+
+#### Put this into the command line in the folder "figures" to get pdf of results
+#dot -Tpdf decision_tree.dot -o decision_tree.pdf
+#open -a preview decision_tree.pdf
